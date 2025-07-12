b3f1e1157bfa7a1ec91c6122a6bfd1948c77d4a5
Manahil Sami <manahil_sami@outlook.com>
2025-07-12 03:00:50 -0400

    created & connected to database

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 const { PORT = 3001 } = process.env;
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
