b50ee0ab87829d56166221cc4046cf35dad2d14a
Manahil Sami <manahil_sami@outlook.com>
2025-07-14 09:13:56 -0400

    user & clothing item model/schemas created

---
+++ models/clothingItem.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  weather: { type: String, enum: ["hot", "warm", "cold"], required: true },
+  imageUrl: {
+    type: String,
+    required: [true, "The image URL field is required."],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("item", clothingItemSchema);
