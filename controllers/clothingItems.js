214132997fe6f2707c56ecccd29a6cd2862c2365
Manahil Sami <manahil_sami@outlook.com>
2025-07-16 15:16:43 -0400

    user controllers created

---
+++ controllers/clothingItems.js
@@ -0,0 +1,47 @@
+const clothingItem = require("../models/item");
+
+//GET /users
+
+const getItems = (req, res) => {
+  clothingItem
+    .find({})
+    .then((items) => {
+      res.status(200).send(items);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message }); //FIX the hard coded 500 error number!
+    });
+};
+
+const createItem = (req, res) => {
+  const { name, avatar } = req.body;
+  // console.log(name, avatar);
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getItems, createItem, deleteItem };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const userRouter = require("./users");
+const clothingItemsRouter = require("./clothingItems");
 
 router.use("/users", userRouter);
 
