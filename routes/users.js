f65ef7649eb325078b8d61d2eec042e06182d9a9
Manahil Sami <manahil_sami@outlook.com>
2025-07-27 12:38:11 -0400

    github actions error fix

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
 const router = require("express").Router();
-const { getUsers, createUser, getUser } = require("../controllers/users");
+// const { getUsers, createUser, getUser } = require("../controllers/users");
 
 module.exports = router;
