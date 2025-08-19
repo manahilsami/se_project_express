9513e6d2828d12d10a13e6b48d7f3297d4be9149
Manahil Sami <manahil_sami@outlook.com>
2025-08-18 22:16:02 -0400

    logging code added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -14,10 +16,14 @@ mongoose
   })
   .catch(console.error);
 
+app.use(requestLogger);
 app.use(cors());
 app.use(express.json());
 app.use("/", mainRouter);
 
+app.use(errorLogger);
+
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
